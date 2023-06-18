cd524df443e846a85e8b9d78aa03601a75cef0c3
safwat-fathi <safwat.rashwan@gmail.com>
2023-06-18 19:19:34 +0300

    add: login & signup pages

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,12 +1,11 @@
 import { GetStaticProps, NextPage } from "next";
 import { NextSeo } from "next-seo";
-import Breadcrumbs from "@/core/components/Breadcrumbs";
-import useBreadcrumbs from "@/lib/hooks/useBreadcrumbs";
-import BreadcrumbItem from "@/core/components/Breadcrumbs/BreadcrumbItem";
 import renderWithLayout from "@/core/HOC/WithLayout";
 import { LayoutsENUM } from "@/core/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import Link from "next/link";
 
 type PageProps = {
   name: string;
@@ -16,33 +15,177 @@ export const getStaticProps: GetStaticProps<PageProps> = async ctx => {
   return {
     props: {
       name: "wwww",
-      ...(await serverSideTranslations(ctx.locale!, ["common", "profile"])),
+      ...(await serverSideTranslations(ctx.locale!, ["common", "signup"])),
     },
   };
 };
 
-const Signup: NextPage<PageProps> = props => {
-  const { t } = useTranslation("profile");
-  const breadcrumbs = useBreadcrumbs();
+const Login: NextPage<PageProps> = props => {
+  const { t } = useTranslation("signup");
 
   return (
     <>
       <NextSeo title="Signup" />
-      <Breadcrumbs>
-        {breadcrumbs &&
-          breadcrumbs.map(breadcrumb => (
-            <BreadcrumbItem
-              key={breadcrumb.path}
-              path={breadcrumb.path}
-              isLast={breadcrumb.isLast}
-              label={breadcrumb.label}
-            />
-          ))}
-      </Breadcrumbs>
-      <h1>{t("title")}</h1>
-      <h2>{props.name}</h2>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="min-h-screen flex w-full max-w-sm mx-auto overflow-hidden bg-white shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+          <div
+            className="hidden bg-cover lg:block lg:w-1/2"
+            style={{
+              backgroundImage:
+                "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')",
+            }}
+          ></div>
+
+          <div className="h-[100vh] flex flex-col justify-center w-full px-6 py-8 md:px-8 lg:w-1/2">
+            <div className="flex justify-center mx-auto">
+              <Image
+                className="w-auto h-7 sm:h-8"
+                width={100}
+                height={100}
+                src="/images/logo.svg"
+                alt=""
+              />
+            </div>
+
+            <div className="">
+              <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
+                Welcome!
+              </p>
+
+              <Link
+                href="#"
+                className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <div className="px-4 py-2">
+                  <Image
+                    src="/icons/google.svg"
+                    width={30}
+                    height={30}
+                    alt="google logo"
+                  />
+                </div>
+
+                <span className="w-5/6 px-4 py-3 font-bold text-center">
+                  Sign in with Google
+                </span>
+              </Link>
+
+              <div className="flex items-center justify-between mt-4">
+                <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+                <span className="text-xs text-center text-gray-500 uppercase dark:text-gray-400">
+                  or signup with email
+                </span>
+                <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
+              </div>
+
+              <div className="mt-4">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                  htmlFor="name"
+                >
+                  Email Address
+                </label>
+                <input
+                  id="name"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="text"
+                />
+              </div>
+
+              <div className="mt-4">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                  htmlFor="EmailAddress"
+                >
+                  Email Address
+                </label>
+                <input
+                  id="EmailAddress"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="email"
+                />
+              </div>
+
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                    htmlFor="password"
+                  >
+                    Password
+                  </label>
+                </div>
+
+                <input
+                  id="password"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="password"
+                />
+              </div>
+
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                    htmlFor="confirm_password"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+
+                <input
+                  id="confirm_password"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="password"
+                />
+              </div>
+
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                    htmlFor="phone"
+                  >
+                    Phone
+                  </label>
+                </div>
+
+                <input
+                  id="phone"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="text"
+                />
+              </div>
+
+              <div className="mt-6">
+                <button
+                  onClick={() => {
+                    console.log("sign in>>>>>");
+                  }}
+                  className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+                >
+                  Signup
+                </button>
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+
+                <Link
+                  href="/signup"
+                  className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                >
+                  or login
+                </Link>
+
+                <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default renderWithLayout(Signup, LayoutsENUM.CLEAN);
+export default renderWithLayout(Login, LayoutsENUM.CLEAN);
