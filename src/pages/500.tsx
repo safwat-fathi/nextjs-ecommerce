8209e7bcad333eb40f35a28f366241741f365201
safwat-fathi <safwat.rashwan@gmail.com>
2023-06-18 19:23:52 +0300

    add: 500 error page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,8 +1,66 @@
 import renderWithLayout from "@/core/HOC/WithLayout";
 import { LayoutsENUM } from "@/core/Layout";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const ServerErrorPage = () => {
-  return <h1>Custom 500 page</h1>;
+  const router = useRouter();
+  const finalSlashIndex = router.asPath.lastIndexOf("/");
+  const previousPath = router.asPath.slice(0, finalSlashIndex);
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+        <div>
+          <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
+            500 error
+          </p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+            Sorry, something went wrong
+          </h1>
+          <p className="mt-4 text-gray-500 dark:text-gray-400">
+            You can try another service or
+            <Link href="/contact-us" role="navigation">
+              contact us
+            </Link>
+            for help
+          </p>
+
+          <div className="flex items-center mt-6 gap-x-3">
+            <Link
+              href={previousPath}
+              role="navigation"
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:rotate-180"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+
+              <span>Go back</span>
+            </Link>
+
+            <Link
+              href="/"
+              className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+            >
+              Take me home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default renderWithLayout(ServerErrorPage, LayoutsENUM.CLEAN);
